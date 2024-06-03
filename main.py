7bf36f6338b2fc0023169892ef365e10bea090bb
Daniel-Hay <127155521+Daniel-Hay@users.noreply.github.com>
2024-06-03 14:43:34 +1000

    Creating A Window

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from tkinter import *
-root = Tk()
-root.mainloop()
 
-print("hello")
+root = Tk()  # create a root widget
+root.title("Tk Example")
+root.configure(background="green4")
+root.minsize(200, 200)  # width, height
+root.maxsize(500, 500)
+root.geometry("300x300+800+300")  # width x height + x + y
+root.mainloop()
