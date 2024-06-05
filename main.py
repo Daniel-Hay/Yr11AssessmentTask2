d6925eda41ea440996ca6b152713f8491687961b
Daniel-control <127155521+Daniel-Hay@users.noreply.github.com>
2024-06-05 23:30:49 +1000

    test of api integration

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from tkinter import *
 import json
+import requests
+
+print("hello")
+response = requests.get("https://nba-stats-db.herokuapp.com/api/playerdata/name/Nikola Jokić") 
+print(response.status_code)
+print(response.json())
 
 root = Tk()  # create a root widget
 root.title("Tk Example")
@@ -8,3 +14,4 @@ root.minsize(200, 200)  # width, height
 root.maxsize(500, 500)
 root.geometry("300x300+800+300")  # width x height + x + y
 root.mainloop()
+
