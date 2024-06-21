244d286788c9058c283b30b9d8fe3a961fdb0350
Daniel-control <127155521+Daniel-Hay@users.noreply.github.com>
2024-06-22 01:27:51 +1000

    Major Update 0.3
    Added random player's data for a random stat

--- main.py
+++ main.py
@@ -11,58 +11,72 @@ app.title("NBA Trivia Game")
 
 ## Use comments and docstrings
 
+
+
+
 class API:    
     def __init__(self, url):
         self.url = url
-
+    
     def openData(self):
         response = requests.get(self.url)
         print(response.status_code)
         self.parseData(response)
 
     def parseData(self, response):
-        players_data = json.loads(response.text)
+        # Change from global after testing to more sophisticated method
+
+
+
+
+
+        global playersData
+        playersData = json.loads(response.text)
         for x in range(0, 7):
-            random_number = random.randrange(51)
-            random_player = players_data["results"][random_number]["player_name"]
-            player = nbaData(random_player)
-            player.retrieveStats()
+            # Change from global after testing to more sophisticated method
+            global randomNumber
+            randomNumber = random.randrange(51)
+            randomPlayer = playersData["results"][randomNumber]["player_name"]
+            print(randomPlayer)
+            player = nbaData(randomPlayer)
+            player.retrieveStat()
             x += 1
 
 class nbaData(API):
     def __init__(self, name):
-        self.stats = f"https://nba-stats-db.herokuapp.com/api/playerdata/name/{name}"
+        self.statsList = ["PTS", "AST", "TRB", "STL", "BLK", "TOV", "PF"]
+        self.name = name
 
-    def retrieveStats(self):
-        player_response = requests.get(self.stats)
-        print(player_response.status_code)
-        player_data = json.loads(player_response.text)
-        print(player_data)
+    def randomStat(self):
+        randomStat = random.choice(self.statsList)
+        print(randomStat)
+        return randomStat
+
+    def retrieveStat(self):
+        randomPlayerStat = playersData["results"][randomNumber][self.randomStat()]
+        playerGamesPlayed = playersData["results"][randomNumber]["games"]
+        randomPlayerStat = round(randomPlayerStat / playerGamesPlayed, 2)
+        print(randomPlayerStat)
         
-
-class main:
-    def choice1():
-        print("hello")
-        nba_API = API("https://nba-stats-db.herokuapp.com/api/playerdata/season/2023")
-        nba_API.openData()
-
-    def choice2():
-        print("yo")
-        nba_API = API("https://nba-stats-db.herokuapp.com/api/playerdata/season/2017")
-        nba_API.openData()
-
+        
 # Make the user choose which year they want to play
 
+def choice(choice):
+    chosenYear = choice
+    print(chosenYear)
+    print("optionmenu dropdown clicked:", choice)
 
-def choice():
-    # Use tkinter to show choices
-    button1 = ctk.CTkButton(app, text="2023", command=main.choice1)
-    button1.pack()
-    button2 = ctk.CTkButton(app, text="2017", command=main.choice2)
-    button2.pack()
+combobox = ctk.CTkOptionMenu(master=app, values=["2023", "2017"], command=choice)
+combobox.pack(padx=20, pady=10)
+combobox.set("2023")  # sets initial value
+
+def start():
+    print("button pressed")
+    nba_API = API(f"https://nba-stats-db.herokuapp.com/api/playerdata/season/{combobox.get()}")
+    nba_API.openData()
+
+startButton = ctk.CTkButton(app, text="Start Game", command=start)
+startButton.pack(pady=10)
 
 if __name__ == '__main__':
-    choice()
-    #main.choice1()
-    #main.choice2()
     app.mainloop()
